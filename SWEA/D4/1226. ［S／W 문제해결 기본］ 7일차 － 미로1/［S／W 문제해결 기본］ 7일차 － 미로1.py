c940634abfd7c16ef55ffbd8744f9eec1ca8b5d4
bsw1206 <toysw1206@gmail.com>
2026-02-27 08:20:29 +0900

    [D4] Title: [S/W 문제해결 기본] 7일차 - 미로1, Time: 86 ms, Memory: 62,848 KB -BaekjoonHub

--- SWEA/D4/1226. ［S／W 문제해결 기본］ 7일차 － 미로1/［S／W 문제해결 기본］ 7일차 － 미로1.py
+++ SWEA/D4/1226. ［S／W 문제해결 기본］ 7일차 － 미로1/［S／W 문제해결 기본］ 7일차 － 미로1.py
@@ -1,33 +1,24 @@
 from collections import deque
-
 dr = [-1, 1, 0, 0]
 dc = [0, 0, -1, 1]
-def bfs(start_r, start_c):
-    q = deque([(start_r, start_c)])
-    visited[start_r][start_c] = True
-    while q:
-        r, c = q.popleft()
-        for i in range(4):
-            nr, nc = r + dr[i], c + dc[i]
-            if 0 <= nr < 16 and 0 <= nc < 16 and not visited[nr][nc] and visited[nr][nc] != 1:
-                if maze[nr][nc] == 3:
-                    return 1
-                if maze[nr][nc] == 0:
-                    visited[nr][nc] = True
-                    q.append((nr, nc))
+
+def dfs(r, c):
+    if maze[r][c] == 3:
+        return True
+    visited[r][c] = True
+    for i in range(4):
+        nr, nc = r + dr[i], c + dc[i]
+        if 0<= nr < 16 and 0 <= nc < 16 and maze[nr][nc] != 1 and not visited[nr][nc]:
+            if dfs(nr, nc):
+                return 1
+            
     return 0
-
-
-
-
-
-
-
-for tc in range(1, 11):
-    T = int(input())
-    maze = [list(map(int, list(input()))) for _ in range(16)]
     
-    visited = [[False] * 16 for _ in range(16)]
-    # start_r, start_c = 1, 1
-    result = bfs(1, 1)
-    print(f'#{T} {result}')
+
+for _ in range(10):
+    tc = int(input())
+    maze = [list(map(int, input())) for _ in range(16)]
+    visited = [[0] * 16 for _ in range(16)]
+    result = dfs(1, 1)
+    print(f'#{tc} {result}')
+    
